ccbf00144f71eba2da991905473196391280f5af
xfranex <franciscojoselopezros@gmail.com>
2025-05-18 20:53:24 +0200

    mensaje de incorrecto del formulario modificado

--- app/Http/Requests/Auth/LoginRequest.php
+++ app/Http/Requests/Auth/LoginRequest.php
@@ -45,7 +45,7 @@ class LoginRequest extends FormRequest
             RateLimiter::hit($this->throttleKey());
 
             throw ValidationException::withMessages([
-                'email' => trans('auth.failed'),
+                'password' => 'El email o la contraseña no son correctos.',
             ]);
         }
 
