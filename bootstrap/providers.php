5517ca11a31f6d636ec7aef534fd8aed4e737984
xfranex <franciscojoselopezros@gmail.com>
2025-06-23 17:51:50 +0200

    API publica desactivada y comentado el provider

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -3,5 +3,5 @@
 return [
     App\Providers\AppServiceProvider::class,
     App\Providers\AuthServiceProvider::class,
-    App\Providers\RouteServiceProvider::class,
+    //App\Providers\RouteServiceProvider::class,
 ];
