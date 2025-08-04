2398a0d7a4cfeab39b43bddd6960b66ec7b916ef
xfranex <franciscojoselopezros@gmail.com>
2025-08-04 22:55:18 +0200

    endpoint de ver series de un producto en concreto hecho

--- app/Http/Controllers/API/ProductoController.php
+++ app/Http/Controllers/API/ProductoController.php
@@ -3,11 +3,14 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Models\Producto;
 
 class ProductoController extends Controller
 {
     public function series($id)
     {
-        
+        $producto = Producto::find($id);
+        $series = $producto->series()->select('series.id', 'series.nombre')->get();  
+        return response()->json($series);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\API\ProductoController;
 
 //prefix api/v1
 Route::prefix('v1')->middleware('token.api')->group(function() {
-    Route::get('carpinterias', [CarpinteriaController::class, 'index']);
-    Route::get('carpinterias/{id}/productos', [CarpinteriaController::class, 'productos']);
+    Route::get('/carpinterias', [CarpinteriaController::class, 'index']);
+    Route::get('/carpinterias/{id}/productos', [CarpinteriaController::class, 'productos']);
     Route::get('/productos/{id}/series', [ProductoController::class,'series']);
 });
