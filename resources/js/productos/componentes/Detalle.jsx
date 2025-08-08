7f8b11e6a94255060002342c7c2cae47b3127fe1
xfranex <franciscojoselopezros@gmail.com>
2025-08-08 04:40:19 +0200

    implementacion de la vista de un producto con su serie en concreto

--- resources/js/productos/componentes/Detalle.jsx
+++ resources/js/productos/componentes/Detalle.jsx
@@ -1,1 +1,15 @@
-...
+import React from "react"
+
+const Detalle = (props) => {
+    return (
+        <div className="w-90 mx-auto">
+            <div className="mb-4 rounded shadow overflow-hidden">
+                <img src={props.imagen} className="img-fluid w-100"/>
+            </div>
+            <div className="text-center">
+                <p className="fs-5 fw-medium">{props.descripcion}</p>
+            </div>
+        </div>
+    )
+}
+export default Detalle
--- resources/js/productos/paginas/Virsualizar.jsx
+++ resources/js/productos/paginas/Virsualizar.jsx
@@ -1,1 +1,23 @@
-...
+import React from "react"
+import useSerieProducto from "../hooks/useSerieProducto"
+import { useParams } from "react-router-dom"
+import Retroceso from "../componentes/Retroceso"
+import Ajax from "../componentes/Ajax"
+import Detalle from "../componentes/Detalle"
+
+const Virsualizar = () => {
+    let { carpinteria } = useParams()
+    let { producto } = useParams()
+    let { serie } = useParams()
+    const {buscando, productoSerie} = useSerieProducto(producto, serie)
+    
+    return (
+        <>
+            <Retroceso ruta={`/productos/carpinterias/${carpinteria}/productos/${producto}/series`}/>
+            <div className="row justify-content-center">
+                {buscando ? <Ajax/> : productoSerie?.descripcion && productoSerie?.imagen ? <Detalle descripcion={productoSerie.descripcion} imagen={`/storage/${productoSerie.imagen}`}/> : "..."}
+            </div>
+        </>
+    )
+}
+export default Virsualizar
