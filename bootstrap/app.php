b366e27e3dac096897986a7569f93bcb6c6cd616
xfranex <franciscojoselopezros@gmail.com>
2025-11-21 15:42:07 +0100

    Documentando bootstrap

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,16 +6,19 @@ use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
+    //configura el enrutamiento de la aplicación en Laravel 12 se realiza aquí por cambios del framework
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        //registro de middleware
         $middleware->alias([
             'token.api' => TokenApi::class,
         ]);
     })
+    //configuración específica de manejo de excepciones
     ->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -1,5 +1,5 @@
 <?php
-
+//devuelve un array con la lista de Service Providers que Laravel cargará automáticamente al iniciar la aplicación
 return [
     App\Providers\AppServiceProvider::class,
     App\Providers\AuthServiceProvider::class,
